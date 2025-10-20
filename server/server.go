1b96b783a5a863566dc76851e742bff677c7fd77
topi314 <git@topi.wtf>
2025-10-21 01:14:58 +0200

    start reloader in dev

--- server/server.go
+++ server/server.go
@@ -56,6 +56,7 @@ func New(cfg Config) (*Server, error) {
 				Funcs(templateFuncs).
 				ParseFS(root.FS(), "templates/*.gohtml")))
 		}
+		reloader.Start(root.FS())
 	} else {
 		subStaticFS, err := fs.Sub(static, "web")
 		if err != nil {
